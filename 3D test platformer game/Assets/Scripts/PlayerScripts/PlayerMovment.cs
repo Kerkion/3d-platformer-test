27d09aa548e81059ec63e46ba87fb586748f34c7
Kerkion <kerkion1984@gmail.com>
2018-03-18 12:39:09 +0100

    Move
    Added simple move function with gravity and simple jump.

---
+++ 3D test platformer game/Assets/Scripts/PlayerScripts/PlayerMovment.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour {
+
+    public float speed;
+    public float jump;
+    public float gravityScale;
+
+    private CharacterController charCon;
+    private Vector3 moveDirection;
+
+    
+    
+
+	// Use this for initialization
+	void Start () {
+        charCon = GetComponent<CharacterController>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    public void Move()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, moveDirection.y, Input.GetAxis("Vertical") * speed);
+
+        if (charCon.isGrounded)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                moveDirection.y = jump;
+            }
+        }
+
+        moveDirection.y = moveDirection.y + Physics.gravity.y * gravityScale;
+
+        charCon.Move(moveDirection * Time.deltaTime);
+
+    }
+
+}
