a7b76c146ead326543fb57a69a2706f1cf3fd27c
Kerkion <kerkion1984@gmail.com>
2018-03-22 22:07:40 +0100

    Added simple Pickups
    Added some simple pick ups ,and some text displaying how much it was collected.

---
+++ 3D test platformer game/Assets/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public int coinValue;
+    public Text coinText;
+
+	public void AddCoin(int coinToAdd)
+    {
+        coinValue += coinToAdd;
+        coinText.text = "Coins: " + coinValue;
+    }
+}
---
+++ 3D test platformer game/Assets/Scripts/GoldCoin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GoldCoin : MonoBehaviour {
+
+    public int value;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().AddCoin(value);
+
+            Destroy(gameObject);
+        }
+    }
+}
